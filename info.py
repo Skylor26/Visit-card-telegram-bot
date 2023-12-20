c0b45148e879d266692712e20535d2b558b9347d
Your Name <you@example.com>
2023-12-20 10:31:12 +0300

    my_frist_commit

---
+++ info.py
@@ -0,0 +1,7 @@
+contacts = """telegram: @skylor0,
+email: sillaavmest@gmail.com
+"""
+
+my_information = ('Hi, my name is Sergey.\nI`m 17.\nI really love programming.\n'
+                  'I am curently studying at Yandex.Practicum on the course "Python in AI from Yandex. '
+                  'Development bots based on neural networks".')
---
+++ main.py
@@ -0,0 +1,43 @@
+import telebot
+from info import my_information, contacts
+
+
+token = '6703162360:AAFpSbUBJ7i6uQIUMgyqiQCK0E8FKTpFkGY'
+bot = telebot.TeleBot(token)
+
+
+help_message = (
+    """ Я умею:
+/help - узнать мои возможности
+/about - расскажу о себе
+/contacts - способы со мной связаться""")
+
+
+@bot.message_handler(commands=['start'])
+def command_start(message):
+    bot.send_message(message.chat.id, text=f'Здравствуй, {message.from_user.first_name}.' + help_message)
+
+
+@bot.message_handler(commands=['help'])
+def command_help(message):
+    bot.send_message(message.chat.id, text=help_message)
+
+
+@bot.message_handler(commands=['about'])
+def command_about(message):
+    bot.send_message(message.chat.id, text=my_information)
+    bot.send_photo(message.chat.id, open('photo_2023-12-18_18-21-22.jpg', 'rb+'))
+
+
+@bot.message_handler(commands=['contacts'])
+def bot_contacts(message):
+    bot.send_message(message.chat.id, text=contacts)
+
+
+@bot.message_handler(content_types=['text'])
+def repeat_message(message):
+    bot.send_message(message.chat.id, f'Пока я не умею обрабатывать присланное вами сообщение."{message.text}", '
+                                      f'пожалуйста используйте команды(посмотреть команды - /help)')
+
+
+bot.polling()
